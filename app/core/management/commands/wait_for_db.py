bcd46f24510d3295a0cfca8a63d438544a2c3283
Nurbolat <nurboturbont@mail.ru>
2023-01-17 17:23:00 +0600

    fix

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,8 @@
+"""
+Command to wait for the database to be available
+"""
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        pass
