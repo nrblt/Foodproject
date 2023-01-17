467fe8f585b35b8eec77b975a07d7967549b805f
Nurbolat <nurboturbont@mail.ru>
2023-01-17 19:29:29 +0600

    formatting fixed

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,17 +1,18 @@
 """
 Command to wait for the database to be available
 """
-import time 
+import time
 
 from psycopg2 import OperationalError as Psycopg2Error
 
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write('Waiting for database...')
-        db_up = False 
+        db_up = False
         while db_up is False:
             try:
                 self.check(databases=['default'])
