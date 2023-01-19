93efdf07a254930e9cd9dbb038325539de2468c4
Nurbolat <nurboturbont@mail.ru>
2023-01-19 16:40:14 +0600

    fix

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,39 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    def test_create_user_model_successful(self):
+        email = "nurbo@example.com"
+        password = "12356789A.a"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
+
+        self.assertEqual(user.email,email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        sample_example = [
+            ["test1@EXAMPLE.com", "test1@example.com"],
+            ["Test2@Example.com", "Test2@example.com"],
+            ["TEST3@EXAMPLE.COM", "TEST3@example.com"],
+            ["test4@example.COM", "test4@example.com"],
+        ]
+        for email, expected in sample_example:
+            user = get_user_model().objects.create_user(email, "sample123")
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('','test123')
+            
+    def test_create_superuser(self):
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123',
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
---
+++ app/user/tests/test_user_api.py
@@ -0,0 +1,87 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+CREATE_USER_URL = reverse("user:create")
+TOKEN_URL = reverse('user:token')
+
+def create_user(**params):
+    return get_user_model().objects.create_user(**params)
+    
+
+class PublicUserApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_user_success(self):
+        payload = {
+            'email': 'test@example.com',
+            'password': 'testpass123',
+            'name': "John"
+        }
+
+        res = self.client.post(CREATE_USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        user = get_user_model().objects.get(email=payload['email'])
+        self.assertTrue(user.check_password(payload['password']))
+        self.assertNotIn('password', res.data)
+
+    def test_user_with_email_exists_error(self):
+        payload = {
+            'email': 'test@example.com',
+            'password': 'testpass123',
+            'name': "John"
+        }
+        create_user(**payload)
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+    
+    def pasword_too_short_error(self):
+        payload = {
+            'email': 'test@example.com',
+            'password': 'wre',
+            'name': "John"
+        }
+
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        user_exists = get_user_model.objects.filter(email=payload['email']).exists()
+        self.assertFalse(user_exists)
+
+    def test_create_token(self):
+        user_details = {
+            'name': "Test Name",
+            'email': 'test@example.com',
+            'password': "testpass123"
+        }
+        create_user(**user_details)
+
+        payload = {
+            'email': 'test@example.com',
+            'password': "testpass123"
+        }
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_create_token_with_wrong_password(self):
+        user_details = {
+            'name': "Test Name",
+            'email': 'test@example.com',
+            'password': "testpass123"
+        }
+        create_user(**user_details)
+        payload = {
+            'email': 'test@example.com',
+            'password': "testpa"
+        }
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
