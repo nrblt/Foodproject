c60b9ab341e8494a5505ff3539ec9c37f41ea054
Nurbolat <nurboturbont@mail.ru>
2023-01-29 01:00:21 +0600

    fix

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,9 +1,9 @@
 from decimal import Decimal
-
-from django.test import TestCase
-from django.contrib.auth import get_user_model
+from unittest.mock import patch
 
 from core import models
+from django.contrib.auth import get_user_model
+from django.test import TestCase
 
 
 def create_user(email="test@exmample.com", password="testpass123"):
@@ -74,3 +74,11 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    @patch('core.models.uuid.uuid4')
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        uuid = 'test-uuid'
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None, 'example.jpg')
+
+        self.assertEqual(file_path, f'uploads/recipe/{uuid}.jpg')
--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -1,8 +1,8 @@
 from decimal import Decimal
-# import tempfile
-# import os
+import tempfile
+import os
 
-# from PIL import Image
+from PIL import Image
 
 from django.contrib.auth import get_user_model
 from django.test import TestCase
@@ -396,40 +396,25 @@ class PrivateRecipeApiTests(TestCase):
 #         self.assertNotIn(s3.data, res.data)
 
 
-# class ImageUploadTests(TestCase):
-#     """Tests for the image upload API."""
+class ImageUploadTests(TestCase):
+    """Tests for the image upload API."""
 
-#     def setUp(self):
-#         self.client = APIClient()
-#         self.user = get_user_model().objects.create_user(
-#             'user@example.com',
-#             'password123',
-#         )
-#         self.client.force_authenticate(self.user)
-#         self.recipe = create_recipe(user=self.user)
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            'user@example.com',
+            'password123',
+        )
+        self.client.force_authenticate(self.user)
+        self.recipe = create_recipe(user=self.user)
 
-#     def tearDown(self):
-#         self.recipe.image.delete()
+    def tearDown(self):
+        self.recipe.image.delete()
 
-#     def test_upload_image(self):
-#         """Test uploading an image to a recipe."""
-#         url = image_upload_url(self.recipe.id)
-#         with tempfile.NamedTemporaryFile(suffix='.jpg') as image_file:
-#             img = Image.new('RGB', (10, 10))
-#             img.save(image_file, format='JPEG')
-#             image_file.seek(0)
-#             payload = {'image': image_file}
-#             res = self.client.post(url, payload, format='multipart')
+    def test_upload_image_bad_request(self):
+        """Test uploading an invalid image."""
+        url = image_upload_url(self.recipe.id)
+        payload = {'image': 'notanimage'}
+        res = self.client.post(url, payload, format='multipart')
 
-#         self.recipe.refresh_from_db()
-#         self.assertEqual(res.status_code, status.HTTP_200_OK)
-#         self.assertIn('image', res.data)
-#         self.assertTrue(os.path.exists(self.recipe.image.path))
-
-#     def test_upload_image_bad_request(self):
-#         """Test uploading an invalid image."""
-#         url = image_upload_url(self.recipe.id)
-#         payload = {'image': 'notanimage'}
-#         res = self.client.post(url, payload, format='multipart')
-
-#         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
